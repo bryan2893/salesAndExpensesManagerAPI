677a7d863f861e2af7a22d7f8ff8b9fb28141b25
Bryan Hernandez Arguello <hernandezbryan2893@gmail.com>
2019-08-07 21:05:30 -0600

    jwt middleware added to supplie_category_routes

--- app/routes/supplie_category_routes.js
+++ app/routes/supplie_category_routes.js
@@ -3,10 +3,10 @@ let supplieCategoryController = require('../controllers/supplie_category');
 let router = express.Router();
 let authMiddleware = require('../middleware/auth');
 
-router.get('/supplie_categories/getAllSupplieCategories',supplieCategoryController.getAllSupplieCategories);
-router.get('/supplie_categories/getSupplieCategory/:categoryCode',supplieCategoryController.getASupplieCategory);
-router.post('/supplie_categories/saveSupplieCategory',supplieCategoryController.saveSupplieCategory);
-router.get('/supplie_categories/deleteSupplieCategory/:categoryCode',supplieCategoryController.deleteSupplieCategory);
-router.post('/supplie_categories/updateSupplieCategory',supplieCategoryController.updateSupplieCategory);
+router.get('/supplie_categories/getAllSupplieCategories',authMiddleware.ensureAuthenticate,supplieCategoryController.getAllSupplieCategories);
+router.get('/supplie_categories/getSupplieCategory/:categoryCode',authMiddleware.ensureAuthenticate,supplieCategoryController.getASupplieCategory);
+router.post('/supplie_categories/saveSupplieCategory',authMiddleware.ensureAuthenticate,supplieCategoryController.saveSupplieCategory);
+router.get('/supplie_categories/deleteSupplieCategory/:categoryCode',authMiddleware.ensureAuthenticate,supplieCategoryController.deleteSupplieCategory);
+router.post('/supplie_categories/updateSupplieCategory',authMiddleware.ensureAuthenticate,supplieCategoryController.updateSupplieCategory);
 
 module.exports = router;
