451919b2331799d6733836c4c5bb4be1833b2a3a
Bryan Hernandez Arguello <hernandezbryan2893@gmail.com>
2019-08-07 21:10:10 -0600

    jwt middleware added to worker_routes

--- app/routes/worker_routes.js
+++ app/routes/worker_routes.js
@@ -3,10 +3,10 @@ let workerController = require('../controllers/worker');
 let router = express.Router();
 let authMiddleware = require('../middleware/auth');
 
-router.get('/workers/getAllWorkers',workerController.getAllWorkers);
-router.get('/workers/getWorker/:workerId',workerController.getAWorker);
-router.post('/workers/saveWorker',workerController.saveWorker);
-router.get('/workers/deleteWorker/:workerId',workerController.deleteWorker);
-router.post('/workers/updateWorker',workerController.updateWorker);
+router.get('/workers/getAllWorkers',authMiddleware.ensureAuthenticate,workerController.getAllWorkers);
+router.get('/workers/getWorker/:workerId',authMiddleware.ensureAuthenticate,workerController.getAWorker);
+router.post('/workers/saveWorker',authMiddleware.ensureAuthenticate,workerController.saveWorker);
+router.get('/workers/deleteWorker/:workerId',authMiddleware.ensureAuthenticate,workerController.deleteWorker);
+router.post('/workers/updateWorker',authMiddleware.ensureAuthenticate,workerController.updateWorker);
 
 module.exports = router;
