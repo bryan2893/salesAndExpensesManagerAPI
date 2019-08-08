b482b3f7d84319390d87f37076a2c92a4d24a381
Bryan Hernandez Arguello <hernandezbryan2893@gmail.com>
2019-08-07 21:02:45 -0600

    jwt middleware added to provider_routes

--- app/routes/provider_routes.js
+++ app/routes/provider_routes.js
@@ -3,10 +3,10 @@ let providerController = require('../controllers/provider');
 let router = express.Router();
 let authMiddleware = require('../middleware/auth');
 
-router.get('/providers/getAllProviders',providerController.getAllProviders);
-router.get('/providers/getProvider/:providerCode',providerController.getAProvider);
-router.post('/providers/saveProvider',providerController.saveProvider);
-router.get('/providers/deleteProvider/:providerCode',providerController.deleteProvider);
-router.post('/providers/updateProvider',providerController.updateProvider);
+router.get('/providers/getAllProviders',authMiddleware.ensureAuthenticate,providerController.getAllProviders);
+router.get('/providers/getProvider/:providerCode',authMiddleware.ensureAuthenticate,providerController.getAProvider);
+router.post('/providers/saveProvider',authMiddleware.ensureAuthenticate,providerController.saveProvider);
+router.get('/providers/deleteProvider/:providerCode',authMiddleware.ensureAuthenticate,providerController.deleteProvider);
+router.post('/providers/updateProvider',authMiddleware.ensureAuthenticate,providerController.updateProvider);
 
 module.exports = router;
