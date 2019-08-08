ffc08dda84355ae42969a42d65d9ea92976b9207
Bryan Hernandez Arguello <hernandezbryan2893@gmail.com>
2019-08-07 20:59:11 -0600

    jwt middleware added to product_categories_routes

--- app/routes/product_category_routes.js
+++ app/routes/product_category_routes.js
@@ -3,10 +3,10 @@ let product_category_controller = require('../controllers/product_category');
 let router = express.Router();
 let authMiddleware = require('../middleware/auth');
 
-router.get('/product_categories/getAllProductCategories',product_category_controller.getAllProductCategories);
-router.get('/product_categories/getProductCategory/:categoryCode',product_category_controller.getAProductCategory);
-router.post('/product_categories/saveProductCategory',product_category_controller.saveProductCategory);
-router.get('/product_categories/deleteProductCategory/:categoryCode',product_category_controller.deleteProductCategory);
-router.post('/product_categories/updateProductCategory',product_category_controller.updateProductCategory);
+router.get('/product_categories/getAllProductCategories',authMiddleware.ensureAuthenticate,product_category_controller.getAllProductCategories);
+router.get('/product_categories/getProductCategory/:categoryCode',authMiddleware.ensureAuthenticate,product_category_controller.getAProductCategory);
+router.post('/product_categories/saveProductCategory',authMiddleware.ensureAuthenticate,product_category_controller.saveProductCategory);
+router.get('/product_categories/deleteProductCategory/:categoryCode',authMiddleware.ensureAuthenticate,product_category_controller.deleteProductCategory);
+router.post('/product_categories/updateProductCategory',authMiddleware.ensureAuthenticate,product_category_controller.updateProductCategory);
 
 module.exports = router;
