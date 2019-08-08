437c15432196bc693f6cb4e3fcdb5cab023a6bb6
Bryan Hernandez Arguello <hernandezbryan2893@gmail.com>
2019-08-07 20:53:25 -0600

    middle ware jwt added to food_routes

--- app/routes/food_routes.js
+++ app/routes/food_routes.js
@@ -3,10 +3,10 @@ let foodController = require('../controllers/food');
 let router = express.Router();
 let authMiddleware = require('../middleware/auth');
 
-router.get('/foods/getAllFoods',foodController.getAllFoods);
-router.get('/foods/getFood/:foodId',foodController.getAFood);
-router.post('/foods/saveFood',foodController.saveFood);
-router.get('/foods/deleteFood/:foodId',foodController.deleteFood);
-router.post('/foods/updateFood',foodController.updateFood);
+router.get('/foods/getAllFoods',authMiddleware.ensureAuthenticate,foodController.getAllFoods);
+router.get('/foods/getFood/:foodId',authMiddleware.ensureAuthenticate,foodController.getAFood);
+router.post('/foods/saveFood',authMiddleware.ensureAuthenticate,foodController.saveFood);
+router.get('/foods/deleteFood/:foodId',authMiddleware.ensureAuthenticate,foodController.deleteFood);
+router.post('/foods/updateFood',authMiddleware.ensureAuthenticate,foodController.updateFood);
 
 module.exports = router;
