0e9e29db32a87b7cf14d6a3f530ea76aba918ac4
Bryan Hernandez Arguello <hernandezbryan2893@gmail.com>
2019-08-07 21:07:45 -0600

    jwt middleware added to supplie_routes

--- app/routes/supplie_routes.js
+++ app/routes/supplie_routes.js
@@ -3,10 +3,10 @@ let suppliesController = require('../controllers/supplie');
 let router = express.Router();
 let authMiddleware = require('../middleware/auth');
 
-router.get('/supplies/getAllSupplies',suppliesController.getAllSupplies);
-router.get('/supplies/getSupplie/:supplieCode',suppliesController.getASupplie);
-router.post('/supplies/saveSupplie',suppliesController.saveSupplie);
-router.get('/supplies/deleteSupplie/:supplieCode',suppliesController.deleteSupplie);
-router.post('/supplies/updateSupplie',suppliesController.updateSupplie);
+router.get('/supplies/getAllSupplies',authMiddleware.ensureAuthenticate,suppliesController.getAllSupplies);
+router.get('/supplies/getSupplie/:supplieCode',authMiddleware.ensureAuthenticate,suppliesController.getASupplie);
+router.post('/supplies/saveSupplie',authMiddleware.ensureAuthenticate,suppliesController.saveSupplie);
+router.get('/supplies/deleteSupplie/:supplieCode',authMiddleware.ensureAuthenticate,suppliesController.deleteSupplie);
+router.post('/supplies/updateSupplie',authMiddleware.ensureAuthenticate,suppliesController.updateSupplie);
 
 module.exports = router;
